7d015addfc7ff07bb93706aae65a88e013889f28
tmwlgey <72266208+tmwlgey@users.noreply.github.com>
2021-05-13 14:48:51 +0800

    Add files via upload

---
+++ 13/event.js
@@ -0,0 +1,10 @@
+
+
+window.changeBackgroundColor = function (){
+  let num = Math.floor(Math.random()*600)
+  document.getElementById('div1').style.Left= num+ 'px';
+  document.getElementById('div1').style.Top= num+ 'px';
+
+}
+window.onload
+
